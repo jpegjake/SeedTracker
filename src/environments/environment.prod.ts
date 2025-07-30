0a9846506eb47077dcf8181a0614ef763c1ad9f8
Jacob <jpegjake@gmail.com>
2025-07-30 16:35:54 -0700

    build settings updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
-export const environment = {
-  production: true
-};
+export class environment {
+  public static readonly production: boolean = true;
+  public static readonly firebase: object = {
+    apiKey: 'AIzaSyCtDUzlnITmwsR3KIRy-4hnMtJJS_VW5VI',
+    authDomain: 'seedtracker-89438.firebaseapp.com',
+    projectId: 'seedtracker-89438',
+    storageBucket: 'seedtracker-89438.firebasestorage.app',
+    messagingSenderId: '192850281419',
+    appId: '1:192850281419:web:9fd9e5d36475281cac6c4e',
+    measurementId: 'G-X6REGYJSTS',
+  };
+}
